e158e2fc785d081a0aa86c2bc10b2f3d30788f3f
Jarvis0328 <jacopopilotta@gmail.com>
2024-09-13 00:09:29 +0200

    added home and song-detail components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { SongDetailComponent } from "./song-detail/song-detail.component";
+import { HomeComponent } from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "song/:id", component: SongDetailComponent },
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "**", redirectTo: "home" },
+];
