cd1784d2fd9fff25ddbe35c148ea034b573898e7
Jarvis0328 <jacopopilotta@gmail.com>
2024-09-13 14:50:49 +0200

    added 15 buttons with link functioning

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,22 @@ import { Router, RouterModule } from "@angular/router";
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  songs: Array<string> = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+  ];
+}
