e8a886926210a68cba6a6615e2933ff86a6459f7
Jarvis0328 <jacopopilotta@gmail.com>
2024-09-13 14:25:07 +0200

    added id as routing param in song-detail

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [RouterModule],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
